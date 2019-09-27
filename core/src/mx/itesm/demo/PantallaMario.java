400006069b3bd0642858d38ca9c439671b46bbb4
tndrpoplap <A01654827@itesm.mx>
2019-09-27 17:05:14 -0500

    A

--- core/src/mx/itesm/demo/PantallaMario.java
+++ core/src/mx/itesm/demo/PantallaMario.java
@@ -1,10 +1,15 @@
 package mx.itesm.demo;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class PantallaMario extends Pantalla {
 
     private Juego juego;
+    private TiledMap mapa;
 
     public PantallaMario(Juego juego){
         this.juego = juego;
@@ -17,12 +22,28 @@ public class PantallaMario extends Pantalla {
     @Override
     public void show() {
 
+        AssetManager manager = new AssetManager();
+
+        manager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+
+        manager.load("map/mapaMario.tmx", TiledMap.class);
+
+        manager.finishLoading(); //
 
 
+        mapa = manager.get("map/mapaMario.tmx");
+
+        rendererMapa = new OrthogonalTiledMapRenderer(mapa);
     }
 
     @Override
     public void render(float delta) {
+
+        borrarPantalla(1,0,0);
+
+        rendererMapa.setView(camara);
+
+        rendererMapa.render();
 
     }
 
