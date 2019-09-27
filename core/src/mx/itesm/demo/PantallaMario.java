b1f1325be045efc5b939c9ade1758cb4c5410817
tndrpoplap <A01654827@itesm.mx>
2019-09-27 16:52:59 -0500

    A

---
+++ core/src/mx/itesm/demo/PantallaMario.java
@@ -0,0 +1,43 @@
+package mx.itesm.demo;
+
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class PantallaMario extends Pantalla {
+
+    private Juego juego;
+
+    public PantallaMario(Juego juego){
+        this.juego = juego;
+
+    }
+
+    private OrthogonalTiledMapRenderer rendererMapa;
+
+
+    @Override
+    public void show() {
+
+
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
